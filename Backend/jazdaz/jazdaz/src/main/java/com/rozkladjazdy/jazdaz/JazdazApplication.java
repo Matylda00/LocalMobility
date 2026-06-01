8ca7803c2a3ce560c11e7eae270218d79e498e77
klimient <lciborowska@student.agh.edu.pl>
2026-06-01 20:42:58 +0200

    jazda z...

---
+++ Backend/jazdaz/jazdaz/src/main/java/com/rozkladjazdy/jazdaz/JazdazApplication.java
@@ -0,0 +1,13 @@
+package com.rozkladjazdy.jazdaz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazdazApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JazdazApplication.class, args);
+	}
+
+}
---
+++ Backend/jazdaz/jazdaz/src/test/java/com/rozkladjazdy/jazdaz/JazdazApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rozkladjazdy.jazdaz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+//class JazdazApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
